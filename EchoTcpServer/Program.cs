f77fd0aed7366599eba2bb55478e4ea24f0acd98
andersbor <anbo@easj.dk>
2018-09-15 08:39:05 +0200

    Add project files.

---
+++ EchoTcpServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EchoTcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
